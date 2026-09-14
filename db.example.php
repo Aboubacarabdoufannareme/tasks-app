0481eee5b5592e412ff8f643db305464c165add4
Fannareme Aboubacar Abdou <125912257+Aboubacarabdoufannareme@users.noreply.github.com>
2026-09-14 19:10:18 +0100

    Update db.example.php

--- db.example.php
+++ db.example.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$db_user = "your_db_username";
-$db_pass = "your_db_password";
-$db_name = "your_database_name";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
 
